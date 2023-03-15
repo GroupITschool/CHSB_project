01d116fbbe2502fe632de2a2f94aa0f8e41baefc
PC680 <ismoilovashahina@gmail.com>
2023-03-15 09:47:02 +0500

    login, register

---
+++ app/src/main/java/com/example/chsb_project/Registration.kt
@@ -0,0 +1,12 @@
+package com.example.chsb_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+}
