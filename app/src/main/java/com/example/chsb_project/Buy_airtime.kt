77a23970af8ecf06b15b6db10916589a1f84744f
PC673 <elshodxushvaqtov07@gmail.com>
2023-03-15 09:53:09 +0500

    Elshod

---
+++ app/src/main/java/com/example/chsb_project/Buy_airtime.kt
@@ -0,0 +1,11 @@
+package com.example.chsb_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buy_airtime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_airtime)
+    }
+}
