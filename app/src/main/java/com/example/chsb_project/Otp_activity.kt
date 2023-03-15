231aba7e1bb82e0d51ae1897b413f54f437e6e50
PC673 <elshodxushvaqtov07@gmail.com>
2023-03-15 09:03:18 +0500

    OTP

---
+++ app/src/main/java/com/example/chsb_project/Otp_activity.kt
@@ -0,0 +1,11 @@
+package com.example.chsb_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Otp_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
